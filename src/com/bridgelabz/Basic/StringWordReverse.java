fe7cf84d6aa5ac05a7fb81022ff80b5d983d8fa4
rawanabhishek <rawatabhishek2012@gmail.com>
2019-08-20 20:42:46 +0530

    basic

---
+++ src/com/bridgelabz/Basic/StringWordReverse.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.Basic;
+
+public class StringWordReverse {
+	public static void main(String[] args) {
+		String str="Hi how are you";
+		String reverseString="";
+		String [] words=str.split(" ");
+		for(int i=0;i<words.length;i++) {
+			String word=words[i];
+			String reverseWord="";
+			for(int j=word.length()-1;j>=0;j--) {
+				reverseWord=reverseWord+word.charAt(j);
+			}
+			reverseString=reverseString+reverseWord+" ";
+		}
+		System.out.println(reverseString);
+
+	}
+}
