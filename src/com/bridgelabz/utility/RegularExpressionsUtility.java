e796d2eda67d2554e2b17f81ae131ee724bd6fc3
rawanabhishek <rawatabhishek2012@gmail.com>
2019-08-26 17:33:30 +0530

    form

--- src/com/bridgelabz/utility/RegularExpressionsUtility.java
+++ src/com/bridgelabz/utility/RegularExpressionsUtility.java
@@ -6,6 +6,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class RegularExpressionsUtility {
+	
+	/**
+	 * Purpose: regex for changew the name and sound from a String 
+	 * 
+	 * @param name user input
+	 * 
+	 */
 
 	public static void Lyric(String str, String Animal, String Sound) {
 		// String tempStr="";
@@ -15,6 +22,12 @@ public class RegularExpressionsUtility {
 
 		System.out.println(replaceString2);
 	}
+	/**
+	 * Purpose: regex for to change the user name mobile number and date from the paragraph
+	 * 
+	 * @param name user input
+	 * 
+	 */
 
 	public  static void paragraph(String str , String name , String fullname ) {
 		DateFormat format =new SimpleDateFormat( "dd/MM/yyyy");
@@ -26,6 +39,68 @@ public class RegularExpressionsUtility {
 		String replaceString4=replaceString3.replaceAll("01/01/2016",format.format(date));
 		
 		System.out.println(replaceString4);
+	}
+	
+	
+	/**
+	 * Purpose: regex for no special charater in string
+	 * 
+	 * @param name user input
+	 * @return if valid true else false
+	 */
+	public boolean isValidName(String name) {
+		Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
+		Matcher matcher = pattern.matcher(name);
+		return matcher.matches();
+
+	}
+
+	/**
+	 * Purpose: regex for email
+	 * 
+	 * @param email user input
+	 * @return if valid true else false
+	 */
+	public boolean isValidEmail(String email) {
+		// TODO Auto-generated method stub ^(.+)@(.+)$
+		Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+
+	/**
+	 * Purpose: regex for international phone number
+	 * 
+	 * @param phone user input
+	 * @return if valid true else false
+	 */
+	public boolean isValidMobile(String phone) {
+		// TODO Auto-generated method stub
+		Pattern pattern = Pattern.compile("^\\+(?:[0-9] ?){6,14}[0-9]$");
+		Matcher matcher = pattern.matcher(phone);
+		return matcher.matches();
+	}
+
+	public boolean isValidUsername(String userid) {
+		// TODO Auto-generated method stub a-zA-Z0-9
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+$");
+		Matcher matcher = pattern.matcher(userid);
+		return matcher.matches();
+	}
+
+	public boolean isValidPassword(String password) {
+		// TODO Auto-generated method stub
+		/*
+		 * (?=.*[a-z]) : This matches the presence of at least one lowercase letter.
+		 * (?=.*d) : This matches the presence of at least one digit i.e. 0-9.
+		 * (?=.*[@#$%]) : This matches the presence of at least one special character.
+		 * ((?=.*[A-Z]) : This matches the presence of at least one capital letter.
+		 * {6,16} : This limits the length of password from minimum 6 letters to maximum
+		 * 16 letters.
+		 */
+		Pattern pattern = Pattern.compile("((?=.*[a-z])(?=.*d)(?=.*[@#$%])(?=.*[A-Z]).{8,16}))");
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
 	}
 
 }
