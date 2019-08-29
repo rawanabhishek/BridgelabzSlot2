9ab9e55e74e153e6f8fc086308a628801700dfe0
rawanabhishek <rawatabhishek2012@gmail.com>
2019-08-29 12:35:12 +0530

    Day 12

--- src/com/bridgelabz/Arrays/BirthDayArray.java
+++ src/com/bridgelabz/Arrays/BirthDayArray.java
@@ -24,48 +24,81 @@ public class BirthDayArray {
 			individual[i]= MonthValue+" / "+DateValue+" / "+YearValue;
 
 		}
+		int count[]=new int [12];
+		 
 		//Arrays.sort(individual);
 		//Arrays.sort(month);
 		for (int i = 0; i < n; i++) {
 			System.out.println(individual[i]);
 		}
 		System.out.println();
-	
+	   
 		for (int i = 0; i < n; i++) {
-			System.out.println(month[i]);
+			//System.out.println(month[i]);
+		if(month[i]==1) {
+			count[0]++;
+			
+			}
+		else if(month[i]==2) {
+			count[1]++;
+			
+			}
+		else if(month[i]==3) {
+			count[2]++;
+			
+			}
+		else if(month[i]==4) {
+			count[3]++;
+			
+			}
+		else if(month[i]==5) {
+			count[4]++;
+			
+			}
+		else if(month[i]==6) {
+			count[5]++;
+			
+			}
+		else if(month[i]==7) {
+			count[6]++;
+			
+			}
+		else if(month[i]==8) {
+			count[7]++;
+			
+			}
+		else if(month[i]==9) {
+			count[8]++;
+			
+			}
+		else if(month[i]==10) {
+			count[9]++;
+			
+			}
+		else if(month[i]==11) {
+			count[10]++;
+			
+			}else if(month[i]==12) {
+				count[11]++;
+				
+				}
+			else{
+			System.out.println("no month has been repeated ");
+		}
 		}
 		
-		int count1=0;
-		for(int i=0 ;i<n-1;i++) {
-			int k = 0;
-			int count=0;
-			for(int j=i+1;j<n;j++) {
-				if(month[i]==month[j] ) {
-					
-					if(count==0  ) {
-						// count1++;
-						count++;
-						System.out.print(individual[i]+ " and "+individual[j]);	
-					}
-//					else if(count==0 ) {
-//						// count1++;
-//						count++;
-//						System.out.print(individual[i]+ " and "+individual[j]);	
-//					}
-					else {
-						System.out.print(" and "+individual[j]);
-						break;
-					}
-					
-				}
+		
+		for(int i=0;i<count.length;i++) {
+			
+				if(count[i]>1 ) {
+					System.out.println("Month "+(i+1)+ " has occured "+count[i]+" times");
 				
-				
-			}
-			if(count>0) {
-				System.out.println(" have same month");
 			}
 			
 		}
+		
+		
+	
 
 	}
 }
--- src/com/bridgelabz/Arrays/MatrixTwoDim.java
+++ src/com/bridgelabz/Arrays/MatrixTwoDim.java
@@ -1,9 +1,88 @@
 package com.bridgelabz.Arrays;
 
+import java.util.Scanner;
+
 public class MatrixTwoDim {
 
 	public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         
+         System.out.println("Enter the value of row and columns for 1 st matrix ");
+         int r1=scanner.nextInt();
+         int c1=scanner.nextInt();
+         
+         System.out.println("Enter the value of  row and column for second matrix  ");
+         int r2=scanner.nextInt();
+         int c2=scanner.nextInt();
+         
+         if(r2==c1) {
+        	 int A[][]=new int [r1][c1];
+        	 int B[][]=new int [r2][c2];
+        	 int result[][]=new int [r1][c2];
+        	 
+        	 
+        	 System.out.println("Enter the value for 1st matrix ");
+        	 
+        	 for(int i=0 ;i<r1 ;i++) {
+        		 for(int j=0;j<c1;j++) {
+        			 System.out.println(i+" "+j+"  : ");
+        			 A[i][j]=scanner.nextInt();
+        		 }
+        		 System.out.println();
+        	 }
+        	 
+        	 System.out.println("the first matrix is ");
+        	 
+        	 for(int i=0 ;i<r1 ;i++) {
+        		 for(int j=0;j<c1;j++) {
+        			 System.out.print(A[i][j]+" ");
+        			 
+        		 }
+        		 System.out.println();
+        	 }
 
+        	 System.out.println("Enter the value for 2nd matrix ");
+        	 
+        	 for(int i=0 ;i<r2 ;i++) {
+        		 for(int j=0;j<c2;j++) {
+        			 System.out.println(i+" "+j+"  : ");
+        			 B[i][j]=scanner.nextInt();
+        		 }
+        		 System.out.println();
+        	 }
+        	 
+             System.out.println("the second matrix is ");
+        	 
+        	 for(int i=0 ;i<r2 ;i++) {
+        		 for(int j=0;j<c2;j++) {
+        			 System.out.print(B[i][j]+" ");
+        			 
+        		 }
+        		 System.out.println();
+        	 }
+        	 
+        	 System.out.println("the multiplication is ");
+        	 
+        	 for(int i=0;i<r1;i++){    
+        		 for(int j=0;j<c2;j++){    
+        		 result[i][j]=0;      
+        		 for(int k=0;k<r2;k++)      
+        		 {      
+        		 result[i][j]+=A[i][k]*B[k][j];      
+        		 }//end of k loop  
+        		 }}
+        	 
+        	 System.out.println();
+        	 System.out.println("Result of  matrix multiplication is : ");
+ 			for (int i = 0; i < r1; i++) {
+ 				for (int j = 0; j < c2; j++) {
+ 					System.out.print(result[i][j]+" ");
+ 				}
+ 				System.out.println();
+ 			}
+
+        	 
+         }
 	}
 
 }
